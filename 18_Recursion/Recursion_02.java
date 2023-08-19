3a254f474bca984ac67ea5b02b78d6d0166b410b
Ayush <ayushkathariyua@ggmail.com>
2023-08-19 22:05:33 +0545

    Recursion_02.java added

---
+++ 18_Recursion/Recursion_02.java
@@ -0,0 +1,19 @@
+/* WAP to print numbers from n to 1 (Decreasing Order) */
+
+public class Recursion_02 {
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
+
+/* Time Complexity: O(n) */
