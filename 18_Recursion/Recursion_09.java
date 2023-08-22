9d602de976dc57b9f65f687e39e0174b89ad063e
Ayush <ayushkathariyua@ggmail.com>
2023-08-22 23:34:48 +0545

    Recursion_09.java added

---
+++ 18_Recursion/Recursion_09.java
@@ -0,0 +1,22 @@
+/* WAP to find first occurence of an elemnet in an array */
+
+public class Recursion_09 {
+    public static int lastOccurance(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurance(arr, key, i + 1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 3, 5 };
+
+        System.out.println(lastOccurance(arr, 3, 0));
+    }
+}
+
+/* Time Complexity: O(n) & Space Complexity: O(n) */
